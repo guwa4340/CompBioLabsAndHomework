616b7ab96b5d8903e313203253035fd82c7db7b4
guwa4340 <gwallach98@gmail.com>
2020-01-26 17:52:01 -0700

    Lab01 and Lab02 Finished

---
+++ Labs/Lab01/Lab01FinalProblem.sh
@@ -0,0 +1,2 @@
+cut -f 2-4 -d , PredPreyData.csv | head -1 > EditedPredPrey.csv  
+cut -f 2-4 -d , PredPreyData.csv | tail >> EditedPredPrey.csv
---
+++ README.txt
@@ -0,0 +1,1 @@
+Included is all of my work for EBIO4420 at the University of Colorado Boulder. 
